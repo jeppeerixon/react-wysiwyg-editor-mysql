115856c724c5bd3bf209888104437eb19d3cae30
Jesper Eriksson <84853548+jeppeerixon@users.noreply.github.com>
2023-04-11 17:22:31 +0200

    Started working on login and register page

--- fronten/src/pages/Start/Start.jsx
+++ fronten/src/pages/Start/Start.jsx
@@ -1,9 +1,15 @@
 import './start.scss';
 
 const Start = () => {
+
+  function handleSignUp() {
+    let regForm = document.querySelector('#registerForm')
+    regForm.style.display = 'block'
+
+  }
   
     return (
-      
+      <>
       <form id="loginForm">
         <h2>Welcome</h2>
         <label>Username
@@ -13,7 +19,20 @@ const Start = () => {
             <input placeholder="Enter Password" type="password" id="password" name="password" required />
         </label>
         <button type="submit">Login</button>
+        <a onClick={handleSignUp}>Not registered? Sign up here!</a>
       </form>
+
+      <form id="registerForm" hidden>
+      <h2>Sign Up</h2>
+      <label>Username
+          <input placeholder="Enter Username" type="text" id="newusername" name="newusername" required />
+      </label>
+      <label>Password
+          <input placeholder="Enter Password" type="password" id="newpassword" name="newpassword" required />
+      </label>
+      <button type="submit">Sign Up</button>
+      </form>
+    </>
 
     );
   };
